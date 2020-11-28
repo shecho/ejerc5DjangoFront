a46a758f1ab3be4ce8e31cbda6604b9e93afda06
shecho <shechovala@gmail.com>
2020-11-28 00:13:09 -0500

    added get student stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  let [student, setStudent] = useState([]);
+  useEffect(() => {
+    getStudens();
+  }, []);
+  const getStudens = async () => {
+    let url = ``;
+    let response = await fetch(url);
+    let res = await response.json();
+    let studentList = res.results;
+    setStudent(studentList);
+    console.log(student);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>todos los estudiantes</div>
     </div>
   );
-}
+};
 
 export default App;
