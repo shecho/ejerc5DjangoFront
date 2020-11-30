4ec1f8de0534b21e038d6d0edeab030c4b4d6d03
shecho <shechovala@gmail.com>
2020-11-30 18:26:52 -0500

    url backend enmdpint stutends

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     getStudens();
   }, []);
   const getStudens = async () => {
-    let url = ``;
+    let url = 'http://127.0.0.1:8000/student/'
     let response = await fetch(url);
     let res = await response.json();
     let studentList = res.results;
